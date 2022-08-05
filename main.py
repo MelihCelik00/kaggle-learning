9287d884f1f6ddb29d5af872329ad6ee2f1a47a9
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-05 12:05:50 +0300

    new queries with pandas

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ melbourne_data = pd.read_csv(melbourne_file_path)
 # print a summary of the data in Melbourne data
 print(melbourne_data.describe())
 
+# What is the average price (rounded to nearest integer)?
+avg_price_amount = round(melbourne_data["Price"].mean())
+
+# As of today, how old is the newest home (current year - the date in which it was built)
+newest_home_age = pd.Timestamp('today').year - melbourne_data["YearBuilt"].max()
+
+print(avg_price_amount)
+print(newest_home_age)
